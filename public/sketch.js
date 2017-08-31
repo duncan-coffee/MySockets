d530ea4d433382f2bc6f1f3aa293d8d9b0a4a356
duncan-coffee <duncanharrist@Duncans-MacBook-Pro.local>
2017-08-31 13:22:19 -0500

    Hopefully

--- public/sketch.js
+++ public/sketch.js
@@ -10,7 +10,7 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('https://aqueous-temple-27893.herokuapp.com');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
@@ -42,7 +42,7 @@ function mouseDragged() {
 function sendmouse(xpos, ypos) {
   // We are sending!
   console.log("sendmouse: " + xpos + " " + ypos);
-  
+
   // Make a little object with  and y
   var data = {
     x: xpos,
